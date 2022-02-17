506f1d113657d34a5c35c368f6ac7bc072b6cf8d
Juliansito <vivaduji2@gmail.com>
2022-02-17 16:38:22 -0500

    Ola

---
+++ ScriptConstruccion.py
@@ -0,0 +1,9 @@
+Motor1=("Baja")
+Motor2=0
+Motor3=90
+if Motor1== ("Baja"):
+    print("Esta en posicion de agarrar algo no muy alto")
+if Motor2== 0:
+    print("La garra no esta en funcion de agarrar algo")
+if Motor3>= 90:
+    print("Acabas de hacer medio gir000o")
